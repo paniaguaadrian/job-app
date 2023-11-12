848803d314262dec9b0e0f1b4fb06fcbc1d6c3c1
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2023-11-12 19:31:13 +0100

    Set up project, home, joblist, useFetch

---
+++ README.md
@@ -0,0 +1,17 @@
+# Expo Router Example
+
+Use [`expo-router`](https://expo.github.io/router) to build native navigation using files in the `app/` directory.
+
+## 🚀 How to use
+
+```sh
+npx create-expo-app -e with-router
+```
+
+## 📝 Notes
+
+-   [Expo Router: Docs](https://expo.github.io/router)
+-   [Expo Router: Repo](https://github.com/expo/router)
+
+https://www.youtube.com/watch?v=mJ3bGvy0WAY&t=1076s
+https://rapidapi.com/letscrape-6bRBa3QguO5/api/jsearch
---
+++ app/_layout.js
@@ -0,0 +1,28 @@
+import { useCallback } from 'react';
+import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [onLayoutRootView]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return <Stack onLayoutRootView={onLayoutRootView} />;
+};
+
+export default Layout;
